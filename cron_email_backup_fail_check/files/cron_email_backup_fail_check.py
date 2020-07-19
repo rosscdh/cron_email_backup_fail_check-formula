5612eaacd3bf6637d76bad2a1126345d46756011
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2020-07-19 22:55:38 +0200

    updates

--- cron_email_backup_fail_check/files/cron_email_backup_fail_check.py
+++ cron_email_backup_fail_check/files/cron_email_backup_fail_check.py
@@ -1,70 +1,68 @@
 #!/usr/bin/python
+import yaml
+import click
+import datetime
+import html2text
+import mailparser
+import imap_tools
+import os, email, imaplib, re, sys, json, base64
 
-import email, imaplib, re, sys, json, base64, os
+from pyzabbix import ZabbixMetric, ZabbixSender
 
-USER_EMAIL = os.getenv("USER_EMAIL")
-USER_PASS_B64 = base64.b64decode(os.getenv("USER_PASS_B64"))
+h2t = html2text.HTML2Text()
+h2t.ignore_links = True
+
 IMAP_ADDR = os.getenv("IMAP_ADDR")
 IMAP_USER = os.getenv("IMAP_USER")
-IMAP_PASS_B64 = base64.b64decode(os.getenv("IMAP_PASS_B64"))
-IMAP_CONN = os.getenv("IMAP_CONN")
-ZABBIX_ALERT_EMAIL = os.getenv("ZABBIX_ALERT_EMAIL")
-SUBJECT_REGEXP = os.getenv("SUBJECT_REGEXP", "Snap:\s*(.+?)\s+failed")
+IMAP_PASS = base64.b64decode(os.getenv("IMAP_PASS_B64")).strip().decode('utf8')
+
+ZABBIX_SERVER = os.getenv("ZABBIX_SERVER", "argus.bienert.tech")
+ZABBIX_PORT = int(os.getenv("ZABBIX_PORT", "10051"))
+
+HOSTS = yaml.load(open('hosts.yaml', 'rb'), Loader=yaml.FullLoader).get('hosts')
+HOST_KEYS = HOSTS.keys()
+
+def check_mail(client: str):
+    criteria = imap_tools.A(date=datetime.date.today())
+    with imap_tools.MailBox(IMAP_ADDR).login(IMAP_USER, IMAP_PASS) as mailbox:
+        for msg in mailbox.fetch(criteria=criteria,
+                                 mark_seen=False):
+            # import pdb;pdb.set_trace()
+            # look over to tuple and try match on host_keys
+            if any(to for to in msg.to if to in HOST_KEYS):
+                # print(f"subject: {msg.subject}, to:{msg.to}, content: {msg.text or h2t.handle(msg.html)}")
+                print(msg.subject, msg.to)
+                print("***"*15)
+    # import pdb;pdb.set_trace()
+    # send_to_zabbix(hostname='hostname1', metrics={})
 
 
-# connect to mailbox,switch to "SNAP" folder and search for emails with "failed"
-# in subject
-user = USER_EMAIL
-pwd = USER_PASS_B64
-conn = imaplib.IMAP4_SSL(IMAP_ADDR)
-conn.login(IMAP_USER, IMAP_PASS_B64)
-conn.select(IMAP_CONN)
+def send_to_zabbix(hostname: str, metrics: dict):
+    """
+    hostname = monkey-host-1
+    metrics = {
+        'test[cpu_usage]': 2,
+        'test[system_status]': "OK",
+    }
+    """
+    # Send metrics to zabbix trapper
+    packet = []
+    for key in metrics:
+        packet.append(
+            ZabbixMetric(hostname, key, metrics[key]),
+        )
 
-# resp, items = conn.uid("search",None, 'All')
-resp, items = conn.uid("search", None, f'(FROM "{ZABBIX_ALERT_EMAIL}")')
+    result = ZabbixSender(zabbix_server=ZABBIX_SERVER,
+                          zabbix_port=ZABBIX_PORT).send(packet)
+    return result
+
+@click.command()
+def mail_check():
+    """
+    App to check todays email backup messages and provide zabbix the status data
+    """
+    check_mail(client='')
 
 
-tdata = []
-items = items[0].split()
-
-for emailid in items:
-    resp, data = conn.uid("fetch", emailid, "(RFC822)")
-    if resp == "OK":
-        email_body = data[0][1].decode("utf-8")
-        mail = email.message_from_string(email_body)
-        if mail["Subject"].find("failed") > 0:
-            # print mail["Subject"]
-            regex1 = f"{SUBJECT_REGEXP}"
-            a = re.findall(regex1, mail["Subject"], re.DOTALL)
-            if a:
-                a = [item.replace("'", "") for item in a]
-                a = [item.replace("\r\n", "") for item in a]
-                a = [item.replace(" ", "_") for item in a]
-                a = [item.replace("|", "_") for item in a]
-                a = [item.replace(".", "_") for item in a]
-                a = [item.replace("-", "") for item in a]
-                a = [item.replace("__", "_") for item in a]
-                a = [item.replace("Processor_", "") for item in a]
-                seen = set()
-                result = []
-                for item in a:
-                    # remove "_for_" and all after it
-                    c = item.split("_for_")[0]
-                    # remove digits
-                    c = "".join([i for i in c if not i.isdigit()])
-                    # limit strings to 36 characters (in order to create zabbix items)
-                    s = c[:36]
-                    # if string ends with "_",remove it
-                    s = re.sub("_$", "", s)
-                    # replace "__" with empty space
-                    s = c.replace("__", "")
-                    s = s[:36]
-                    s = re.sub("_$", "", s)
-                    if s not in seen:
-                        seen.add(s)
-                        result.append(s)
-                        output = " ".join(result)
-                        output.join(result)
-                        # create LLD JSON output
-                        tdata.append({"{#JOB}": output, "{#NAME}": item})
-print(json.dumps({"data": tdata}, indent=4))
+if __name__ == '__main__':
+    mail_check()
